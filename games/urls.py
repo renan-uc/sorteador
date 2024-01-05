336a92e9589be22b46409de4c14d6faa130b6b26
Renan <renan.cruz10@gmail.com>
2024-01-05 16:45:35 -0300

    Novas rotas

--- games/urls.py
+++ games/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.sorteio)
+    path('', views.sorteio),
+    path('lotofacil', views.lotofacil, name='lotofacil'),
+    path('megasena', views.megasena, name='megasena'),
+    path('quina', views.quina, name='quina'),
 ] 
--- games/views.py
+++ games/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 def sorteio(request):
     return render(request, 'sorteio.html')
+
+def lotofacil(request):
+    return render(request, 'lotofacil.html')
+
+def megasena(request):
+    return render(request, 'megasena.html')
+
+def quina(request):
+    return render(request, 'quina.html')
